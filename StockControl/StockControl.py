ae225dee0eebf60792436ba59c1ea07f3b06d21e
deconduo <deconduo@tcd.ie>
2013-07-09 11:11:54 +0100

    Added class and method templates

--- StockControl/StockControl.py
+++ StockControl/StockControl.py
@@ -2,6 +2,84 @@
 
 '''Classes'''
 
+class StockItem(object):
+    def __init__(self):
+        uniqueID
+        clientName
+        pricePerUnit
+        warehouseNumber
+        
+    def __str__(self):
+        pass
+    
+    def stockAdd(self):
+        pass
+    
+    def stockMultiply(self):
+        pass
+    
+    def CalculateStorageCost(self):
+        pass
+
+
+class CD(StockItem):
+    def __init__(self):
+        title
+        dateReleased
+        genre
+        artist
+    
+    def __str__(self):
+        pass
+    
+    def GetDateReleased(self):
+        pass
+    
+    def IsAPreRelease(self):
+        pass
+    
+    
+class Book(StockItem):
+    def __init__(self):
+        title
+        dateReleased
+        genre
+        author
+
+    def __str__(self):
+        pass
+    
+    def GetDateReleased(self):
+        pass
+    
+    def IsAPreRelease(self):
+        pass
+    
+class StockRepository(object):
+    def __init__(self):
+        pass
+    
+    def EnterStock(self):
+        pass
+    
+    def MoveStock(self):
+        pass
+    
+    def DeleteStock(self):
+        pass
+    
+    def AddAllStockInWarehouse(self):
+        pass
+    
+class StockException(Exception):
+    def __init__(self):
+        pass
+    
+class TestClass(object):
+    def __init__(self):
+        pass    
+
+
 '''Functions'''
 
 '''Main Program'''

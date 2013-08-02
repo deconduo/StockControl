c62726182f8d10280c7edf5099603ec61ac006b8
deconduo <deconduo@tcd.ie>
2013-08-02 18:59:49 +0100

    Added new tests.

--- StockControl/Tests/StockItemTest.py
+++ StockControl/Tests/StockItemTest.py
@@ -1,12 +1,23 @@
 import unittest
 from ..Classes.StockItemClass import StockItem
+from ..Classes.StockExceptionClass import StockException
 
 class StockItemTest(unittest.TestCase):
     def setUp(self):
         self.testItem1 = StockItem("Test Client 1", 1, 11.00)
         self.testItem2 = StockItem("Test Client 2", 2, 12.00)
+        self.notAString = []
 
     def tearDown(self):
+        pass
+    
+    def testInitClient(self):
+        self.assertRaises(StockException, StockItem, self.notAString, 1, 11.00)
+    
+    def testInitWarehouse(self):
+        pass
+    
+    def testInitPrice(self):
         pass
 
     def testAddItems(self):
